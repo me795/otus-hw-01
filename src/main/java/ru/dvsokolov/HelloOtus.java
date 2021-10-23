646e88be7e264ec2f80b3d855c501738f57a8cc2
sokolovdmvl <user@mail.ru>
2021-10-23 19:42:08 +0300

    init gradle project

--- src/main/java/ru/dvsokolov/HelloOtus.java
+++ src/main/java/ru/dvsokolov/HelloOtus.java
@@ -1,8 +1,19 @@
 package ru.dvsokolov;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloOtus {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        List<Integer> testList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            testList.add(i);
+        }
+        for (List<Integer> integers : Lists.partition(testList, 3)) {
+            System.out.println(integers);
+        }
     }
 }

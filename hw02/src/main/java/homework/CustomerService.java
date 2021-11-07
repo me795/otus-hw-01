d7a498d7d95b1a5b06357ae449062baecd9f441a
sokolovdmvl <user@mail.ru>
2021-11-07 20:10:03 +0300

    changes for hw02

--- hw02/src/main/java/homework/CustomerService.java
+++ hw02/src/main/java/homework/CustomerService.java
@@ -1,23 +1,37 @@
 package homework;
 
 
-import java.util.Map;
+import java.util.*;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final NavigableMap<Customer, String> customerMap = new TreeMap<>(new ComparatorByScores());
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> value = customerMap.firstEntry();
+        NavigableMap<Customer, String> copyMap = new TreeMap<>(new ComparatorByScores());
+        copyMap.put(value.getKey().clone(), value.getValue());
+
+        return copyMap.firstEntry();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+
+        Map.Entry<Customer, String> value = customerMap.higherEntry(customer);
+        if (value == null){
+            return null;
+        }else {
+            NavigableMap<Customer, String> copyMap = new TreeMap<>(new ComparatorByScores());
+            copyMap.put(value.getKey().clone(), value.getValue());
+
+            return copyMap.firstEntry();
+        }
     }
 
     public void add(Customer customer, String data) {
-
+        customerMap.put(customer, data);
     }
 }

cde12f89e1a5fb89605f1bfd406af6b8de2cc82b
sokolovdmvl <user@mail.ru>
2022-01-25 21:59:07 +0300

    hw init

--- hw09/src/main/java/ru/dvsokolov/myorm/jdbc/mapper/DataTemplateJdbc.java
+++ hw09/src/main/java/ru/dvsokolov/myorm/jdbc/mapper/DataTemplateJdbc.java
@@ -6,7 +6,6 @@ import ru.dvsokolov.core.repository.executor.DbExecutor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.dvsokolov.crm.model.Client;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -16,8 +15,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 
 /**
  * Сохратяет объект в базу, читает объект из базы
@@ -25,70 +22,16 @@ import java.util.stream.Collectors;
 public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     private final DbExecutor dbExecutor;
-    private final ru.dvsokolov.myorm.jdbc.mapper.EntitySQLMetaData entitySQLMetaData;
+    private final EntitySQLMetaData entitySQLMetaData;
 
-    private static final Logger log = LoggerFactory.getLogger(DataTemplateJdbc.class);
-
-    public DataTemplateJdbc(DbExecutor dbExecutor, ru.dvsokolov.myorm.jdbc.mapper.EntitySQLMetaData entitySQLMetaData) {
+    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
         this.dbExecutor = dbExecutor;
         this.entitySQLMetaData = entitySQLMetaData;
     }
 
     @Override
     public Optional<T> findById(Connection connection, long id) {
-        var entity = dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectByIdSql(), List.of(id),
-                rs -> {
-
-                    try {
-                        var entityField = entitySQLMetaData.getClass().getDeclaredField("entityClassMetaData");
-                        entityField.setAccessible(true);
-                        EntityClassMetaData entityClassMetaData = (EntityClassMetaData) entityField.get(entitySQLMetaData);
-
-                        String name = entityClassMetaData.getName();
-                        System.out.println(name);
-                        Constructor<T> constructor = entityClassMetaData.getConstructor();
-                        Object obj = constructor.newInstance();
-
-                        try {
-                            if (rs.next()) {
-
-                                List<Field> fieldList = entityClassMetaData.getAllFields();
-
-                                for (var field : fieldList){
-                                    try {
-                                        var fieldName = field.getName();
-                                        field.setAccessible(true);
-                                        field.set(obj,rs.getObject(fieldName));
-                                    } catch (IllegalAccessException e) {
-                                        throw new RuntimeException("Field not found");
-                                    }
-                                }
-                                return obj;
-                            }
-                        } catch (SQLException e) {
-                            log.error(e.getMessage(), e);
-                            throw new RuntimeException("SQL error: "+ e.getMessage());
-                        }
-
-                    
-                    } catch (NoSuchFieldException e) {
-                        log.error(e.getMessage(), e);
-                        throw new RuntimeException("Field not found");
-                    } catch (IllegalAccessException e) {
-                        log.error(e.getMessage(), e);
-                        throw new RuntimeException(e);
-                    } catch (InvocationTargetException e) {
-                        log.error(e.getMessage(), e);
-                        throw new RuntimeException(e);
-                    } catch (InstantiationException e) {
-                        log.error(e.getMessage(), e);
-                        throw new RuntimeException(e);
-                    }
-
-                    return null;
-                });
-
-        return (Optional<T>) entity;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -98,32 +41,11 @@ public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     @Override
     public long insert(Connection connection, T client) {
-        var entityClassMetaData = getEntityClassMetaData(client);
-        List<Field> fieldList = entityClassMetaData.getFieldsWithoutId();
-        List<Object> valueList = new ArrayList<>();
-        for (var field : fieldList){
-            try {
-                var fieldName = field.getName();
-                field.setAccessible(true);
-                var value = field.get(client);
-                valueList.add(value);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Field not found");
-            }
-        }
-
-        String sql = entitySQLMetaData.getInsertSql();
-        var clientId = dbExecutor.executeStatement(connection, entitySQLMetaData.getInsertSql(), valueList);
-
-        return clientId;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void update(Connection connection, T client) {
         throw new UnsupportedOperationException();
-    }
-
-    private EntityClassMetaData getEntityClassMetaData(T obj){
-         return new EntityClassMetaDataImpl(obj.getClass());
     }
 }

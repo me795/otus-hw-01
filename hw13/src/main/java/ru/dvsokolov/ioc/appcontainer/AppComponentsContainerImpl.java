82c1f4f41f4110b71a1a8797d78bc7d0e71768e7
sokolovdmvl <user@mail.ru>
2022-02-22 22:49:40 +0300

    hw13 IoC

--- hw13/src/main/java/ru/dvsokolov/ioc/appcontainer/AppComponentsContainerImpl.java
+++ hw13/src/main/java/ru/dvsokolov/ioc/appcontainer/AppComponentsContainerImpl.java
@@ -1,9 +1,12 @@
 package ru.dvsokolov.ioc.appcontainer;
 
+import ru.dvsokolov.ioc.appcontainer.api.AppComponent;
 import ru.dvsokolov.ioc.appcontainer.api.AppComponentsContainer;
 import ru.dvsokolov.ioc.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
@@ -16,7 +19,25 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+
+        var methods = configClass.getDeclaredMethods();
+        Arrays.stream(methods)
+                .filter(m -> m.isAnnotationPresent(AppComponent.class))
+                .sorted((m1, m2) -> Integer.compare(m1.getAnnotation(AppComponent.class).order(), m2.getAnnotation(AppComponent.class).order()))
+                .forEach(m -> {
+                    var params = m.getParameters();
+                    var paramsReady = Arrays.stream(params)
+                            .map(p ->
+                                    getAppComponent(p.getType())
+                            ).toArray();
+                    try {
+                        var component = m.invoke(configClass.getDeclaredConstructor().newInstance(), paramsReady);
+                        appComponents.add(component);
+                        appComponentsByName.put(m.getAnnotation(AppComponent.class).name(), component);
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +48,24 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+
+        var interfaceComponentClass = (componentClass.isInterface())
+                ? componentClass
+                : Arrays.stream(componentClass.getInterfaces())
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Interfaces not found"));
+
+        return (C) appComponents.stream()
+                .filter(cmp -> Arrays.stream(cmp.getClass().getInterfaces())
+                        .findFirst().orElseThrow(() -> new RuntimeException("Interfaces not found"))
+                        .getName().equals(interfaceComponentClass.getName()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No such component"));
+
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
